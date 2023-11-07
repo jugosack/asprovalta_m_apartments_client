e94e5bef05973bda8e8ee52dca0ccfea8072f3e6
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-07 23:49:18 +0100

    Add logo

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/style.css';
+import logo from '../images/logo.png'; // Import your image
 
 const Body = () => (
-  <header className="header">
-    <span className="header-title">It&apos;s not Perfect... It&apos;s Paradise </span>
-    <div className="header-book-btn-space">
-      <span className="header-book-btn">
-        <Link to="/signin" className="link-sign">Sign In</Link>
-      </span>
-      <span className="space">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-      <span className="header-book-btn-signup">
-        <Link to="/signup" className="link-sign">Sign Up</Link>
-      </span>
+  <>
+    <div className="section-1">
+      <img src={logo} alt="logo.png" />
     </div>
-  </header>
+    <header className="header">
+      <span className="header-title">It&apos; s not Perfect... It&apos; s Paradise </span>
+      <div className="header-book-btn-space">
+        <span className="header-book-btn">
+          <Link to="/signin" className="link-sign">Sign In</Link>
+        </span>
+        <span className="space">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <span className="header-book-btn-signup">
+          <Link to="/signup" className="link-sign">Sign Up</Link>
+        </span>
+      </div>
+    </header>
+
+  </>
 );
 
 export default Body;
