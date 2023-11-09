7a133f5c88dafee6c1b5a88dfa93589937e67adb
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-10 00:17:56 +0100

    Add changes for buttons

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,19 +6,20 @@ import logo from '../images/logo.png'; // Import your image
 const Body = () => (
   <>
     <div className="section-1">
-      <img src={logo} alt="logo.png" />
+      <img className="logoA" src={logo} alt="logo.png" />
+      <div className="header-book-btn-space">
+        <div className="header-book-btn">
+          <Link to="/signin" className="link-sign">Sign In</Link>
+        </div>
+        <span className="space">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <div className="header-book-btn-signup">
+          <Link to="/signup" className="link-signup">Sign Up</Link>
+        </div>
+      </div>
     </div>
     <header className="header">
       <span className="header-title">It&apos; s not Perfect... It&apos; s Paradise </span>
-      <div className="header-book-btn-space">
-        <span className="header-book-btn">
-          <Link to="/signin" className="link-sign">Sign In</Link>
-        </span>
-        <span className="space">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        <span className="header-book-btn-signup">
-          <Link to="/signup" className="link-sign">Sign Up</Link>
-        </span>
-      </div>
+
     </header>
 
   </>
