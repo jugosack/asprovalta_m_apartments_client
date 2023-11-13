b55725ed9010d35539044a2ac05a269d708fbb48
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-13 14:17:41 +0100

    Add fontawsome, facebook and google icons

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,13 @@ const Body = () => (
   <>
     <div className="section-1">
       <img className="logoA" src={logo} alt="logo.png" />
+      <div className="contacts">
+        <div className="phone"> +389 71 292 496 </div>
+        <div className="email"> asprovaltam@yahoo.com</div>
+        <div className="phone"> FB </div>
+        <div className="phone"> gogle plus </div>
+      </div>
+
       <div className="header-book-btn-space">
         <div className="header-book-btn">
           <Link to="/signin" className="link-sign">Sign In</Link>
