680b8c409adf365cc921253b2da85948d5397de9
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-13 16:20:38 +0100

    Add changes in navbar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
 import logo from '../images/logo.png'; // Import your image
 
 const Body = () => (
@@ -8,10 +12,32 @@ const Body = () => (
     <div className="section-1">
       <img className="logoA" src={logo} alt="logo.png" />
       <div className="contacts">
-        <div className="phone"> +389 71 292 496 </div>
-        <div className="email"> asprovaltam@yahoo.com</div>
-        <div className="phone"> FB </div>
-        <div className="phone"> gogle plus </div>
+        <div className="phone">
+          <FontAwesomeIcon icon={faPhone} className="phone-icon" />
+          +389 71 292 496
+        </div>
+        <div className="email">
+          <FontAwesomeIcon icon={faEnvelope} className="envelope-icon" />
+          asprovaltam@yahoo.com
+        </div>
+        <div className="facebook">
+          <a
+            href="https://www.facebook.com/asprovaltam/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} className="facebook-icon" />
+          </a>
+        </div>
+        <div className="location">
+          <a
+            href="https://www.google.com/maps/place/Asprovalta+M+Apartments/@40.7240395,23.7063886,17z/data=!3m1!4b1!4m16!1m9!4m8!1m0!1m6!1m2!1s0x14a91f22438b3a3d:0xcee56071c2b751c8!2sAsprovalta+M+Apartments,+Leof.+Egnatias,+Asprovalta+570+21,+Greece!2m2!1d23.7089635!2d40.7240355!3m5!1s0x14a91f22438b3a3d:0xcee56071c2b751c8!8m2!3d40.7240355!4d23.7089635!16s%2Fg%2F11h4ql_hpc?entry=ttu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="location-icon" />
+          </a>
+        </div>
       </div>
 
       <div className="header-book-btn-space">
